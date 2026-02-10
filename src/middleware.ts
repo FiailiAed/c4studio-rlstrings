7b6c7a008e2d208a089959d19c0a3b874baba6db
fiailiaed <1822lax@gmail.com>
2026-02-09 23:51:46 -0500

    admin view updated. favicon added. dashboard.css and DashboardLayout.astro created. Temp components/ui/ files added: Button,ButtonGallery,CardGallery,Footer,Hero,InputGallery,Nav,TypographySection.

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ export const onRequest = clerkMiddleware((auth, context) => {
     const { redirectToSignIn, userId } = auth();
     const { request } = context;
 
+    // 1. If it's an admin route and they aren't logged in, send them to sign-in
     if (isAdminRoute(request) && !userId) {
         return redirectToSignIn();
     }
